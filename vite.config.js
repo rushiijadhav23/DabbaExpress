a70796d7428e04367146f04f1f4e06963a3a44d1
Rushikesh <rushijadhav1423@gmail.com>
2025-04-05 19:23:25 +0530

    added form to the waitlist

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,26 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['36bf-2401-4900-7dcb-2750-40db-b375-7f3a-cd66.ngrok-free.app'],
+  },
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     allowedHosts: ['36bf-2401-4900-7dcb-2750-40db-b375-7f3a-cd66.ngrok-free.app'],
+//     proxy: {
+//       "/api": {
+//         target: "https://spade.tools",
+//         changeOrigin: true,
+//         secure: false,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+// })
